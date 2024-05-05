2b5436d51a4fa36df87ad7058f5dd12dd7008797
rodgersxy <rodgersny99@gmail.com>
2024-05-05 18:58:54 +0300

    feat: scraping multiple links within the same page

--- wbs/app.py
+++ wbs/app.py
@@ -1,21 +1,27 @@
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
 
-website = "https://subslikescript.com/movie/Titanic-120338"
+root = 'https://subslikescript.com'
+website = f'{root}/movies'
 
 result = requests.get(website)
-
-content = result.text
-
-soup = BeautifulSoup(content, 'lxml')
-# print(soup.prettify())
+soup = BeautifulSoup(result.text, 'html.parser')
 
 box = soup.find('article', class_='main-article')
 
-title = box.find('h1').get_text()
+links = []
 
-transcript = box.find('div', class_='full-script').get_text(strip=True, separator=' ')
+for link in box.find_all('a', href=True):
+    links.append(link['href'])
 
-with open(f'{title}.txt', 'w') as file:
-    file.write(transcript)
+for link in links:
+    website = f'{root}{"/" if not link.startswith("/") else ""}{link}'  # Adding a slash if missing
+    result = requests.get(website)
+    soup = BeautifulSoup(result.text, 'html.parser')
+    box = soup.find('article', class_='main-article')
 
+    title = box.find('h1').get_text()
+    transcript = box.find('div', class_='full-script').get_text(strip=True, separator=' ')
+
+    with open(f'{title}.txt', 'w') as file:
+        file.write(transcript)
