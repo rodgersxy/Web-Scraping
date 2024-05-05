4e0cc8b66ed4fcc40fa9409341e33daaee0cb835
rodgersxy <rodgersny99@gmail.com>
2024-05-05 18:21:54 +0300

    gitignore file

--- wbs/app.py
+++ wbs/app.py
@@ -8,5 +8,14 @@ result = requests.get(website)
 content = result.text
 
 soup = BeautifulSoup(content, 'lxml')
-print(soup.prettify())
+# print(soup.prettify())
+
+box = soup.find('article', class_='main-article')
+
+title = box.find('h1').get_text()
+
+transcript = box.find('div', class_='full-script').get_text(strip=True, separator=' ')
+
+with open(f'{title}.txt', 'w') as file:
+    file.write(transcript)
 
