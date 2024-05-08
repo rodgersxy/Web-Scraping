d6f39c1512bb69ecf3f7017d26550ab28ad83644
rodgersxy <rodgersny99@gmail.com>
2024-05-08 16:52:40 +0300

    feat: headless mode to scrap books-title,author and lenth

---
+++ wbs/selenium-bot/scrap_audible.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+import pandas as pd
+import time
+
+web = "https://www.audible.com/search"
+path = "/home/rodgers/Downloads/chrome-linux64/chrome"
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = path
+chrome_options.add_argument('--headless')
+
+driver = webdriver.Chrome(options=chrome_options)
+driver.get(web)
+
+# Find the container element by its class name
+container = driver.find_element(By.CLASS_NAME, 'adbl-impression-container')
+products = container.find_elements(By.XPATH, './/li[contains(@class, "productListItem")]')
+
+book_title = []
+book_author = []
+book_length = []
+
+for product in products:
+    book_title.append(product.find_element(By.XPATH, './/h3[contains(@class, "bc-heading")]').text)
+    book_author.append(product.find_element(By.XPATH, './/li[contains(@class, "authorLabel")]').text)
+    book_length.append(product.find_element(By.XPATH, './/li[contains(@class, "runtimeLabel")]').text)
+
+driver.quit()
+
+df_books = pd.DataFrame({'title': book_title, 'author': book_author, 'length': book_length})
+df_books.to_csv('books_audible.csv', index=False)
