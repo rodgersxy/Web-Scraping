497c4b93a9d81313de445928e578c343c4b277b7
rodgersxy <rodgersny99@gmail.com>
2024-05-15 21:58:57 +0300

    feat: User-agent

--- wbs/scrapy/spider_project/spider_project/spiders/audible.py
+++ wbs/scrapy/spider_project/spider_project/spiders/audible.py
@@ -4,7 +4,13 @@ from scrapy.http import Request
 class AudibleSpider(scrapy.Spider):
     name = "audible"
     allowed_domains = ["www.audible.com"]
-    start_urls = ["https://www.audible.com/search"]
+    #start_urls = ["https://www.audible.com/search"]
+
+
+    # user-agent
+    def start_requests(self):
+        yield scrapy.Request(url="https://www.audible.com/search", callback=self.parse,
+                       headers={'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'})
 
     def parse(self, response):
         product_container = response.xpath('//div[@class="adbl-impression-container "]//li[contains(@class, "productListItem")]')
@@ -16,6 +22,7 @@ class AudibleSpider(scrapy.Spider):
                 'title': book_title,
                 'author': book_author,
                 'length': book_length,
+                'User-Agent': response.request.headers['User-Agent']
             }
 
         # Follow pagination buttons
@@ -24,7 +31,8 @@ class AudibleSpider(scrapy.Spider):
         next_page_url = pagination.xpath('.//span[contains(@class, "nextButton")]/a/@href').get()
 
         if next_page_url:
-            yield response.follow(url=next_page_url, callback=self.parse)
+            yield response.follow(url=next_page_url, callback=self.parse,
+                                  headers={'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'})
 
 
         # if pagination:
