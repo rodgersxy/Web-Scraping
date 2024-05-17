c0bf3ce561240491a1226e7f4e329e5f4094ed68
rodgersxy <rodgersny99@gmail.com>
2024-05-17 17:41:46 +0300

    feat: paginating rest of the pages and store in json

--- wbs/scrapy/spider_project/spider_project/spiders/transcripts.py
+++ wbs/scrapy/spider_project/spider_project/spiders/transcripts.py
@@ -2,17 +2,21 @@ import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 
-
 class TranscriptsSpider(CrawlSpider):
     name = "transcripts"
     allowed_domains = ["subslikescript.com"]
     start_urls = ["https://subslikescript.com/movies"]
+    custom_settings = {
+        'DOWNLOAD_DELAY': 1,
+    }
 
-    rules = (Rule(LinkExtractor(restrict_xpaths=("//ul[@class='scripts-list']/a")), callback="parse_item", follow=True),)
+    rules = (
+        Rule(LinkExtractor(restrict_xpaths=("//ul[@class='scripts-list']/a")), callback="parse_item", follow=True),
+        Rule(LinkExtractor(restrict_xpaths=("//a[@rel='next']",))),  # next page
+    )
 
     def parse_item(self, response):
         article = response.xpath("//article[@class='main-article']")
-        
         yield {
             'title': article.xpath("./h1/text()").get(),
             'plot': article.xpath("./p/text()").get(),
